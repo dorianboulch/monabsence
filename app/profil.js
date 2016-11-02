47261492fa3d5c825cb0de78ae803b624cda83f5
Gwennael JEAN <gwennael.jean@gmail.com>
2016-11-02 21:08:27 +0100

    feat: sauvegarde de la signature

--- app/profil.js
+++ app/profil.js
@@ -4,7 +4,9 @@
  * and open the template in the editor.
  */
 
-var fs = require('fs');
+var electron = require('electron');
+var remote = electron.remote;
+var fs = remote.require('fs');
 
 var storage = require('electron-json-storage');
 
@@ -12,14 +14,27 @@ storage.get('profil', function(error, data) {
     if (error) throw error;
 
     jQuery('body').find('[name=nom]').val(data.nom);
-    jQuery('body').find('[name=prenom]').val(data.prenom); 
+    jQuery('body').find('[name=prenom]').val(data.prenom);
+    jQuery('body').find('[name=signature]').val(data.signature);
+    jQuery('#signature_visuel').append(jQuery('<img />').attr('src', data.signature));
 });
 
 jQuery('#save').on("click", function () {
     storage.set('profil', {
-        nom     : jQuery('body').find('[name=nom]').val(),
-        prenom  : jQuery('body').find('[name=prenom]').val()
+        nom         : jQuery('body').find('[name=nom]').val(),
+        prenom      : jQuery('body').find('[name=prenom]').val(),
+        signature   : jQuery('body').find('[name=signature]').val()
     });
     
     window.location.href = 'index.html';
+});
+
+jQuery('#signature').on("click", function () {
+    remote.dialog.showOpenDialog(remote.getCurrentWindow(), {
+        title: "Selectionnez votre signature",
+        properties: ['openFile']
+    }, function (filenames) {
+        jQuery('body').find('[name=signature]').val(filenames[0]);
+        jQuery('#signature_visuel').append(jQuery('<img />').attr('src', filenames[0]));
+    });
 });
